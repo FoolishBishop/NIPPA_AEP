fb514355cf7b1d8e9c45eb6134c4dbb0f1a754f3
mecatronica_2 <mecatronica_2@iMac-de-mecatronica-2.local>
2023-09-01 16:24:29 -0400

    bme_sensor

---
+++ BME280.py
@@ -0,0 +1,29 @@
+## IMPORTS
+import board
+import time
+from adafruit_bme280 import basic as adafruit_bme280
+
+##CONFIGURATION VARIABLES
+i2c = board.I2C()   # uses board.SCL and board.SDA  
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+##VARIABLES
+bme280.sea_level_pressure = 1013.25
+temperature = bme280.temperature
+humidity = bme280.relative_humidity
+pressure = bme280.pressure
+altitude = bme280.altitude
+
+##METHODS
+def getTemperature():
+    return temperature
+
+def getHumidity():
+    return humidity
+
+def getPressure():
+    return pressure
+
+def getAltitude():
+    return altitude
+
