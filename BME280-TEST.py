8bb1c1de603259cd4fbbd980a838a363ee8df11e
MrSquids <51426751+MrSquids@users.noreply.github.com>
2023-09-08 17:11:26 -0400

    Update BME280-TEST.py

--- BME280-TEST.py
+++ BME280-TEST.py
@@ -2,16 +2,25 @@ import board
 import time
 from adafruit_bme280 import basic as adafruit_bme280
 
-# Create sensor object, using the board's default I2C bus.
+##CONFIGURATION VARIABLES
 i2c = board.I2C()   # uses board.SCL and board.SDA
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+data = open("temperature.csv","a")
 
-# change this to match the location's pressure (hPa) at sea level
-bme280.sea_level_pressure = 1013.25
+##VARIABLES
+bme280.sea_level_pressure = 1013.25 # change this to match the location's pressure (hPa) at sea level
+temperature = bme280.temperature
+humidity = bme280.relative_humidity
+pressure = bme280.pressure
+altitude = bme280.altitude
 
-while True:
-    print("\nTemperature: %0.1f C" % bme280.temperature)
-    print("Humidity: %0.1f %%" % bme280.relative_humidity)
-    print("Pressure: %0.1f hPa" % bme280.pressure)
-    print("Altitude = %0.2f meters" % bme280.altitude)
-    time.sleep(2)
+##TEST DE INICIO --Borrar Despues--
+print("\nTemperature: %0.1f C" % bme280.temperature)
+print("Humidity: %0.1f %%" % bme280.relative_humidity)
+print("Pressure: %0.1f hPa" % bme280.pressure)
+print("Altitude = %0.2f meters" % bme280.altitude)
+
+with data as f_object:
+    writer_object = writer(f_object)
+    writer_object.writerow([temperature, humidity, pressure, altitude])
+    f_object.close()
