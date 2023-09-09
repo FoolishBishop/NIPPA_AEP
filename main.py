701f037bd2d0531ba32d0d6f25e1aadc204b95ac
Jorge Enciso <jorged.encyso@gmail.com>
2023-09-08 23:06:21 -0400

    inheritance for icm and class object for bme, added multiprocessing to main

---
+++ main.py
@@ -0,0 +1,18 @@
+import multiprocessing as mp
+
+from ICM20948 import *
+from BME280_1 import *
+
+if __name__ == '__main__':
+    #define sensors
+    bme = BME280()
+    icm = ICM_20948()
+    while True:
+        #write to csv file in parallel
+        p1 = mp.Process(target = bme.to_csv())
+        p2 = mp.Process(target = icm.to_csv())
+
+        p1.start()
+        p2.start()
+
+        #define other functionalities
