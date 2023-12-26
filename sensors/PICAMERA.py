c576006f421247b78d8ada081936ad26159c2e56
Jorgedavyd <jorged.encyso@gmail.com>
2023-12-25 22:40:52 -0300

    camera settings

--- sensors/PICAMERA.py
+++ sensors/PICAMERA.py
@@ -1,10 +1,12 @@
 #implement pi camera
 import picamera
-import multiprocessing as mp
 
 class PICAMERA:
     def get_data_camera(self, name):
         with picamera.PiCamera() as camera:
             camera.resolution = (512,512)
+            camera.iso = 1200
+            camera.shutter_speed = 1000  
+            camera.exposure_mode = 'night' 
             camera.capture(f'/home/pi/Desktop/NIPPA_AEP/{self.date}/video/' + str(name) + '.jpg')
             camera.close()
