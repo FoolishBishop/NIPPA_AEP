507c55e88395d19a4976e82869482cd999ede901
Jorgedavyd <jorged.encyso@gmail.com>
2023-12-27 01:41:56 -0300

    hola

--- prueba_plot.py
+++ prueba_plot.py
@@ -4,14 +4,12 @@ from matplotlib.animation import FuncAnimation
 import datetime as dt
 from lora_reciever import Receiver
 
-x = []
-y = [[]*4]
+fig, ax = plt.subplots()
 
-fig, ax = plt.subplots(ncols = 1, nrows = 4)
 names = ['Temperature', 'Humidity', 'Pressure', 'Altitude']
 boundary = [(0,50), (0,1000), (0,2000), (0,200)]
 
-def animate(i,x,y):
+def animate(i,x,y, index):
     receiver = Receiver()
     packet = None
     packet = receiver.receiver.receive()
@@ -19,19 +17,21 @@ def animate(i,x,y):
         packet = str(packet, 'utf-8')[1:-1]
         data = [float(values) for values in packet.split(',')][1:]
 
-        for index in range(4):
-            variable = data[index]
-            
-            x.append(dt.datetime.now().strftime('%H:%M:%S'))
-            y[index].append(variable)
+        variable = data[index]
+        
+        x.append(dt.datetime.now().strftime('%H:%M:%S'))
+        y.append(variable)
 
-            x = x[-20:]
-            y[index] = y[index][-20:]
-            ax[index].clear()
-            ax[index].plot(x,y[index])
-            ax[index].set_title(f'{names[index]} over time')
-            ax[index].set_ylim(boundary[index])
+        x = x[-20:]
+        y = y[-20:]
+        ax.clear()
+        ax.plot(x,y[index])
+        ax.set_title(f'{names[index]} over time')
+        ax.set_ylim(boundary[index])
 
         plt.xticks(rotation=45)
-
-FuncAnimation(fig, animate, interval=100, repeat=False, fargs=(x,y,0, (0,60)))
+def make_plot(index):
+    x = []
+    y = []
+    anim = FuncAnimation(fig, animate, interval=100, repeat=False, fargs=(x,y, index))
+    plt.show()
