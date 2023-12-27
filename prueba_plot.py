ff077d674bbb280dacedfdf3dbe91fda40ac8dbc
Jorgedavyd <jorged.encyso@gmail.com>
2023-12-27 00:52:43 -0300

    prueba_plot

--- prueba_plot.py
+++ prueba_plot.py
@@ -7,27 +7,33 @@ from lora_reciever import Receiver
 x = []
 y = []
 
-fig, ax = plt.subplots()
-
-def animate(i,x,y):
+fig, ax = plt.subplots(ncols = 1, nrows = 4)
+names = ['Temperature', 'Humidity', 'Pressure', 'Altitude']
+def animate(i,x,y, index: int, boundary: tuple):
     receiver = Receiver()
     packet = None
     packet = receiver.receiver.receive()
     if packet is not None:
         packet = str(packet, 'utf-8')[1:-1]
-        data = [float(values) for values in packet.split(',')]
-        altitude = data[3]
-    
+        data = [float(values) for values in packet.split(',')][1:]
+        variable = data[index]
+        
         x.append(dt.datetime.now().strftime('%H:%M:%S'))
-        y.append(altitude)
+        y.append(variable)
 
         x = x[-20:]
         y = y[-20:]
 
-        ax.clear()
-        ax.plot(x,y)
+        ax[index].clear()
+        ax[index].plot(x,y)
+        ax[index].set_title(f'{names[index]} over time')
+        ax[index].set_ylim(boundary)
+
         plt.xticks(rotation=45)
 
-ani = FuncAnimation(fig, animate, interval=1000, repeat=False, fargs=(x,y))
+ani = FuncAnimation(fig, animate, interval=100, repeat=False, fargs=(x,y,0, (0,60)))
+ani = FuncAnimation(fig, animate, interval=100, repeat=False, fargs=(x,y,1, (0,1000)))
+ani = FuncAnimation(fig, animate, interval=100, repeat=False, fargs=(x,y,0, (0,20000)))
+ani = FuncAnimation(fig, animate, interval=100, repeat=False, fargs=(x,y,0, (0,200)))
 
 plt.show()
